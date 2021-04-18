050731c1d07d9466b11b54098d8b3a0650bb0ce5
Huilin Zou <56774880+huilin-zou@users.noreply.github.com>
2021-04-18 16:03:44 -0400

    Add files via upload

---
+++ package-info.java
@@ -0,0 +1,78 @@
+package group_project;
+/**
+ * 
+ */
+/**
+ * @author zouhu
+ *
+ */
+package group_project;
+
+import edu.cuny.csi.csc330.lab5.User;
+
+public class Admin{
+	private String name;
+	private int ID;
+	private int age;
+	
+	public login() {
+		
+	}
+	
+	public manageUsers() {
+		
+	}
+	
+	public manageAppointment() {
+		
+	}
+	
+	public viewDoctor() {
+		
+	}
+	
+	public appointment() {
+		
+	}
+	
+	public sendReminderToPatient() {
+		
+	}
+	
+	public manageWaittingList() {
+		
+	}
+	
+	 
+}
+
+public class Appointment{
+	private int appointmentNo;
+	private int appointmentDate;
+	
+	public cancelAppointment() {
+		
+	}
+	
+	public addAppointment() {
+		
+	}
+	public searchAppointment() {
+	
+}
+	public changeAppointment() {
+	
+}
+
+	
+}
+
+public class Update extends User{
+	public update_Speciality_certification_experience{
+		
+	}
+	
+	public add_Speciality_certification_experience{
+			
+	}
+}
